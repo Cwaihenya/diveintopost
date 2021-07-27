3bd900d345875f15de63a7597e50fb666171551d
Cwaihenya <cwaihenya.gmail.com>
2021-07-27 10:11:49 +0900

    issue1

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,9 +15,22 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+  
+  def destroy
+    if current_user.id==@agenda.user_id
+       @agenda.destroy
+       @agenda.team.assigns.each do |assign|
+         ContactMailer.contact_mail(assign.user.email,@agenda).deliver
+       end
+       #ContactMailer.contact_mail(@agenda).deliver
+       redirect_to dashboard_url, notice: "agenda deleted "
+    else
+      redirect_to dashboard_url, notice: "you can't delete other owner's agenda"
     end
   end
 
